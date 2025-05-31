5afe4ba246ef02db70448d393b8c8f2d276d39f3
rohanjoshi15 <rohan.joshi1155@gmail.com>
2025-05-31 11:20:13 +0530

    Updated with QoS profiles for both subscriptions and one publisher

--- src/number_square/src/number_publisher.cpp
+++ src/number_square/src/number_publisher.cpp
@@ -1,14 +1,25 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/int32.hpp"
 
-using std::placeholders::_1;
-
 class NumberPublisher : public rclcpp::Node 
 {
 public:
     NumberPublisher() : Node("number_publisher"), num(1) 
     {
-        pub = this->create_publisher<std_msgs::msg::Int32>("sending_nums", 10);
+        // gonna give QOS Profiles here - 
+        rclcpp::QoS qos_profile(rclcpp::QoSInitialization::from_rmw(rmw_qos_profile_default));
+        qos_profile
+            .reliability(RMW_QOS_POLICY_RELIABILITY_RELIABLE)// we can also giv best effort here 
+            .durability(RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL)// also can give transient local
+            .history(RMW_QOS_POLICY_HISTORY_KEEP_LAST)
+            .keep_last(15) //here the number in the bracket is the Queue size, Queue size is only when the history is keep last
+            .deadline(rclcpp::Duration::from_seconds(2))
+            .lifespan(rclcpp::Duration::from_seconds(4))
+            .liveliness(RMW_QOS_POLICY_LIVELINESS_AUTOMATIC)
+            .liveliness_lease_duration(rclcpp::Duration::from_seconds(3));
+
+        pub = this->create_publisher<std_msgs::msg::Int32>("sending_nums", qos_profile);
+
         timer = this->create_wall_timer(
             std::chrono::seconds(1),
             std::bind(&NumberPublisher::publish_number, this));
--- src/number_square/src/number_square.cpp
+++ src/number_square/src/number_square.cpp
@@ -3,14 +3,28 @@
 
 class NumberSubscriber : public rclcpp::Node {
 public:
+
+//We can give different QOS Settingds to different subscribers , subscribed to the same topic, like how i have shown here rn
     NumberSubscriber() : Node("number_subscriber") 
     {
+        // so ive given Reliable and transient local for sub_1
+        rclcpp::QoS qos_1(rclcpp::KeepLast(10));
+        qos_1
+            .reliability(RMW_QOS_POLICY_RELIABILITY_RELIABLE)
+            .durability(RMW_QOS_POLICY_DURABILITY_TRANSIENT_LOCAL);
+
+        // and here beest effort and volatile for sub_2
+        rclcpp::QoS qos_2(rclcpp::KeepLast(5));
+        qos_2
+            .reliability(RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT)
+            .durability(RMW_QOS_POLICY_DURABILITY_VOLATILE);
+
         sub_1 = this->create_subscription<std_msgs::msg::Int32>(
-            "sending_nums", 10,
+            "sending_nums", qos_1,
             std::bind(&NumberSubscriber::callback_1, this, std::placeholders::_1));
 
-        sub_2 = this->create_subscription<std_msgs::msg::Int32>(                        //just to show more than one sub can be subbed to one topic
-            "sending_nums", 10,
+        sub_2 = this->create_subscription<std_msgs::msg::Int32>(
+            "sending_nums", qos_2,
             std::bind(&NumberSubscriber::callback_2, this, std::placeholders::_1));
     }
 
@@ -18,18 +32,17 @@ private:
     void callback_1(const std_msgs::msg::Int32::SharedPtr msg) 
     {
         int square = msg->data * msg->data;
-        RCLCPP_INFO(this->get_logger(), "Received: %d, Square: %d", msg->data, square);
+        RCLCPP_INFO(this->get_logger(), "[QoS1] Received: %d, Square: %d", msg->data, square);
     }
 
     void callback_2(const std_msgs::msg::Int32::SharedPtr msg) 
     {
-        int square = msg->data * msg->data * msg->data;
-        RCLCPP_INFO(this->get_logger(), "Received: %d, Cube: %d", msg->data, square);
+        int cube = msg->data * msg->data * msg->data;
+        RCLCPP_INFO(this->get_logger(), "[QoS2] Received: %d, Cube: %d", msg->data, cube);
     }
 
     rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr sub_1;
     rclcpp::Subscription<std_msgs::msg::Int32>::SharedPtr sub_2;
-
 };
 
 int main(int argc, char *argv[]) 
